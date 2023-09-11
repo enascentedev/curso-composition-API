f5c2f8a950e08d10218180f01052d87862637161
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-09-11 14:44:42 -0300

    criando componentes usando composition API

---
+++ exemplo/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { reactive } from "vue";
+import Progress from "./components/Progress.vue";
+
+let courses = reactive([
+	{
+		title: "Javascript",
+		done: true,
+	},
+	{
+		title: "React",
+		done: false,
+	},
+	{
+		title: "vue",
+		done: true,
+	},
+]);
+let newCourse = { done: false };
+function addCourse() {
+	courses.push(newCourse);
+	newCourse = {};
+}
+</script>
+
+<template>
+	<div>
+		<h1>Lista de cursos</h1>
+		<ul>
+			<li v-for="course in courses" :key="course.title">
+				{{ course.title }}
+			</li>
+		</ul>
+		<br />
+		<input v-model="newCourse.title" type="text" />
+
+		<button @click="addCourse">Adiconar</button>
+		<Progress :courses="courses" />
+	</div>
+</template>
+
+<style scoped></style>
---
+++ exemplo/src/components/Progress.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["courses"]);
+const courseDone = computed(() => {
+	return props.courses.filter((course) => course.done).length;
+});
+</script>
+<template>
+	<div>
+		{{ ` ${courseDone} cursos completos de um total de ${courses.length}` }}
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
